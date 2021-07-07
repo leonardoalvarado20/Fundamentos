78244682e1eef89b8695b5c672e16bf552935d11
Leonardo David Alvarado Cornejo <86163124+leonardoalvarado20@users.noreply.github.com>
2021-07-07 01:34:54 -0500

    Mostrar Matriz
    Mostrara la matriz 1 y matriz 2

--- B2.- Github para el desarrollo colaborativo/Menu.cpp
+++ B2.- Github para el desarrollo colaborativo/Menu.cpp
@@ -28,6 +28,44 @@ int main(){
 				//clrscr
 				system("cls");
 				
+				//FUNCION MOSTRAR 2 Matriz 1x10
+				
+				int mostrar();{
+					float A[10][10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+					float B[10][10]={2.3,4.5,6.5,0.5,6.1,5.5,1.2,2.4,6.5,9.1};
+					
+					//Mostrar las matrice
+					
+					cout<<"\n\n";
+				    cout<<"\t\t\t\t\t //============================//"<<endl;
+					cout<<"\t\t\t\t\t //     Mostrando matriz 1     //"<<endl;
+					cout<<"\t\t\t\t\t //============================//"<<endl;
+					cout<<"\n\n";
+					
+					
+					for(int i=0;i<1;i++){
+						for(int j=0;j<10;j++){
+							cout<<"\t"<<" ["<< A[i][j]<<"] ";
+						
+						}
+					    cout<<"\n\n";
+					}
+					cout<<"\n\n";
+				    cout<<"\t\t\t\t\t //============================//"<<endl;
+					cout<<"\t\t\t\t\t //     Mostrando matriz 2     //"<<endl;
+					cout<<"\t\t\t\t\t //============================//"<<endl;
+					cout<<"\n\n";															
+					
+					
+					for(int i=0;i<1;i++){
+						for(int j=0;j<10;j++){
+							cout<<"\t"<<" ["<< B[i][j]<<"] ";
+						
+						}
+					    cout<<"\n\n";
+					}	
+					cout<<"\n\n";					 														
+				}				
 				system("pause");
 			    system("cls");
 		        break;

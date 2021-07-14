84b8fb3a020ff5a6986407bd8b0a395406e01efc
Leonardo David Alvarado Cornejo <86163124+leonardoalvarado20@users.noreply.github.com>
2021-07-14 11:43:07 -0500

    Mostrar Indice Corporal  IMC
    Mostrar el indice corporal y felicitar si se encuentra bien,  o darle alguna recomendación si su indice es el correcto.

--- C2.-2-Uso de Clases y Objetivo/principal.cpp
+++ C2.-2-Uso de Clases y Objetivo/principal.cpp
@@ -12,14 +12,15 @@ class Persona{
 		int aa,ma,da,an,mn,dn,a,m,d;
 		float peso;
 		float altura;
+		float IMC;
 			
 	public: //Metodos
-		Persona(int,string,int,int,int,float,float);//Contructor
+		Persona(int,string,int,int,int,float,float,float);//Contructor
 		void leer();
     	    				 
 };
 //Constructor
-Persona::Persona(int _cedula,string _nombre,int _an,int _mn,int _dn,float _peso,float _altura){
+Persona::Persona(int _cedula,string _nombre,int _an,int _mn,int _dn,float _peso,float _altura,float _IMC){
 	cedula = _cedula;
 	nombre = _nombre;
 	an = _an;
@@ -27,6 +28,7 @@ Persona::Persona(int _cedula,string _nombre,int _an,int _mn,int _dn,float _peso,
 	dn = _dn;
 	peso = _peso;
 	altura = _altura;   
+	IMC = _IMC;
 }
 
 void Persona::leer(){
@@ -50,6 +52,7 @@ string nombre;
 int aa,ma,da,an,mn,dn,a,m,d;
 float peso;
 float altura;
+float IMC;
 
 int main(){	         
     
@@ -91,14 +94,14 @@ int main(){
                  	cout<<"\n";
                  	cout<<"\t\t3 - Dia: ";cin>>dn;
                  	cout<<"\t\t  - Mes: ";cin>>mn;
-                 	cout<<"\t\t  - Anios ";cin>>an;                
+                 	cout<<"\t\t  - Anios: ";cin>>an;                
                  	cout<<"\n"; 
                  	cout<<"\t\t4 - ";cin>>peso;
                  	cout<<"\n";				
                  	cout<<"\t\t5 - ";cin>>altura;
                  	cout<<"\n";						 	                 	
                  	system("pause");
-                 	Persona p1 = Persona(cedula,nombre,an,mn,dn,peso,altura);
+                 	Persona p1 = Persona(cedula,nombre,an,mn,dn,peso,altura,IMC);
                 	p1.leer();
  
                  }          				
@@ -166,7 +169,41 @@ int main(){
 			case 3:
 				//clrscr
 				system("cls");
-		
+
+				int corporal();{
+					
+					cout<<"\n";
+					cout<<"\t\t\t\t         //========================================//"<<endl; 
+ 	                cout<<"\t\t\t\t         //        Mostrar  Indice Comporal        //"<<endl;
+ 	                cout<<"\t\t\t\t         //========================================//"<<endl; 	                
+ 	                cout<<"\n\n";
+ 	                cout<<"\t\t\tEl peso es : "<<peso<<" y la altura es : "<<altura<<" "<<"\n\n"; 
+					 IMC = peso / (altura*altura);
+					cout<<"\t\t\tEL IMC es de : "<<IMC<<"\n\n"; 
+					if(IMC<18){
+						cout<<"\t\t\tTienes un Peso bajo : Te encuentra en Excelente Salud "<<"\n\n";
+					}else{
+						if(IMC>=18 && IMC<=24.9){
+							cout<<"\t\t\tTienes un Peso Normal : Tu Salud es Buena siga asi manteniendo su Salud "<<"\n\n";
+					    }else{
+						    if(IMC>=25 && IMC<=26.9){
+							    cout<<"\t\t\tTienes un Sobrepeso : Tu Salud indica que debe hacer un mejoramiento en su alimentacion y ejercicio"<<"\n\n";
+						    }else{
+						    	if(IMC>=27 && IMC<=29.9){
+						    		cout<<"\t\t\tTienes una Obesidad grado 1 : Tu Salud esta en un rango bajo de riesgo "<<"\n\n";
+								}else{
+									if(IMC>=30 && IMC<=39.9){
+										cout<<"\t\t\tTienes una Obesidad grado 2 : Tu Salud esta en un rango medio de riesgo "<<"\n\n";
+									}else{
+										if(IMC>=40){
+											cout<<"\t\t\tTienes una Obesidad grado 3 : Tu Salud esta en riesgo Muerte "<<"\n\n";
+										}	
+									}	
+								}						    								    		
+						    }
+					    }
+					} 	         	                					                
+			    }			
 				system("pause");
 			    system("cls");
 		        break;						    
@@ -192,7 +229,8 @@ int main(){
 					archivo<<"Fecha Nacimiento : "<<dn<<"/"<<mn<<"/"<<an<<"\n";  
 					archivo<<"Peso : "<<peso<<"(Kg)"<<"\n";
 					archivo<<"Estatura : "<<altura<<"(Mt)"<<"\n"; 
-					archivo<<"Usted tiene : "<<a<<"  anios "<<m<<" meses y "<<d<< " dias"<<"\n";   					       	                
+					archivo<<"Edad : "<<a<<"  anios "<<m<<" meses y "<<d<< " dias"<<"\n";
+					archivo<<"Indice Masa Corporal : "<<IMC<<"\n";    					       	                
 	                archivo.close(); //Cerrar el archivo
                   }	               	
                 }
